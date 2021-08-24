5a1e8b7713f78cb39c6373b7bcc53e4afef8ec1f
mursitaffandi <mursitaffandi@gmail.com>
2021-08-24 14:44:12 +0700

    out vehicle

--- staff/models.py
+++ staff/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
 # Create your models here.
 class vehicle(models.Model):
     vehicle_name = models.CharField(max_length=16)
-    vehicle_type = models.CharField(max_length=64)
+    vehicle_wheels = models.PositiveIntegerField(default=2, validators=[MinValueValidator(1), MaxValueValidator(16)])
--- staff/views.py
+++ staff/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
 
 # Create your views here.
 def index(req):
-    if req.POST:
-        plat = req.POST['name']
+    if 'plat' in req.POST and 'type' in req.POST:
+        plat = req.POST['plat']
         wheel = req.POST['type']
-        models.tugas.objects.create(name=plat, type=wheel)
+        models.vehicle.objects.create(vehicle_name = plat, vehicle_wheels = wheel)
+        print(req.POST)
+
+    if 'outId' in req.GET:
+        id = req.GET['outId']
+        models.vehicle.objects.filter(pk=id).delete()
         
     dataFetch = models.vehicle.objects.all()
     return render(req, 'index.html', {
