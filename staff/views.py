7d500fe25de4f75167476f366cc94b5fdc631af9
mursitaffandi <mursitaffandi@gmail.com>
2021-09-02 22:14:09 +0700

    method action vehicle

--- staff/views.py
+++ staff/views.py
@@ -2,18 +2,21 @@ from django.shortcuts import render, redirect
 from . import models
 
 # Create your views here.
-def index(req):
-    if 'plat' in req.POST and 'type' in req.POST:
-        plat = req.POST['plat']
-        wheel = req.POST['type']
-        models.vehicle.objects.create(vehicle_name = plat, vehicle_wheels = wheel)
-        print(req.POST)
-
-    if 'outId' in req.GET:
-        id = req.GET['outId']
-        models.vehicle.objects.filter(pk=id).delete()
-        
+def index(req):    
     dataFetch = models.vehicle.objects.all()
     return render(req, 'index.html', {
 		'list_vehicle' : dataFetch,
 		})
+
+def exit(req):
+    if 'outId' in req.POST:
+        id = req.POST['outId']
+        models.vehicle.objects.filter(pk=id).delete()
+    return redirect('/')
+
+def enter(req):
+    if 'plat' in req.POST and 'type' in req.POST:
+        plat = req.POST['plat']
+        wheel = req.POST['type']
+        models.vehicle.objects.create(vehicle_name = plat, vehicle_wheels = wheel)
+    return redirect('/')    
