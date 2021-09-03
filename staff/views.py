abe03579311daf75cc9e8c28e796845ef6bf0dab
mursitaffandi <mursitaffandi@gmail.com>
2021-09-03 10:34:02 +0700

    view proper json reponse

--- staff/views.py
+++ staff/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from . import models
+from django.http import JsonResponse
 
 # Create your views here.
 def index(req):    
@@ -12,11 +13,15 @@ def exit(req):
     if 'outId' in req.POST:
         id = req.POST['outId']
         models.vehicle.objects.filter(pk=id).delete()
-    return redirect('/')
+
+    dataFetch = models.vehicle.objects.all()
+    return JsonResponse({'status': 200, 'message' : 'success', 'data' : dataFetch})
 
 def enter(req):
     if 'plat' in req.POST and 'type' in req.POST:
         plat = req.POST['plat']
         wheel = req.POST['type']
         models.vehicle.objects.create(vehicle_name = plat, vehicle_wheels = wheel)
-    return redirect('/')    
+    
+    dataFetch = models.vehicle.objects.all()
+    return JsonResponse({'status': 200, 'message' : 'success', 'data' : dataFetch})
